a56fd59ca9738672c450b10b24ebce28fcaeaedd
Cherono-mercy <cheronom142@gmail.com>
2023-11-09 15:18:22 +0800

    Importing Header component to our application

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {useThemeHook} from "./GlobalComponents/ThemeProvider"
-
+import Header from './Components/Header';
 
 function App() {
 
   const [theme] = useThemeHook();
   return (
     <main className={theme? "bg-black": "bg-light-2"}>
-      App
+      <Header/>
     </main>
   )
 }
