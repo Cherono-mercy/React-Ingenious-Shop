e1bd242459acd32786f3a4aedca9628070c28cd2
Cherono-mercy <cheronom142@gmail.com>
2023-11-09 17:52:01 +0800

    Creating search bar feature in the Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, InputGroup, FormControl, Form } from "react-bootstrap";
+import { useThemeHook } from "../GlobalComponents/ThemeProvider";
+import { BiSearch } from "react-icons/bi";
 
 function Home() {
+  const [theme] = useThemeHook();
+  const [searchInput, setSearchInput] = useState("");
   return (
-    <div>
-        Home Page
-        </div>
-  )
+    <Container className="py-4">
+      <Row className="justify-content-center">
+        <Col xs={10} md={7} lg={6} xl={4} className="mb-3 mx-auto text-center">
+          <h1 className={theme ? "text-light my-5" : "text-black my-5"}>
+            Search Products
+          </h1>
+          <InputGroup className="mb-3">
+            <InputGroup.Text className={theme? "bg-black text-dark-primary" : "bg-light text-light-primary"}>
+            <BiSearch size="2rem" />
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Search"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className={theme? "bg-light-black text-light" : "bg-light text-black"}
+            />
+          </InputGroup>
+          {searchInput}
+        </Col>
+        
+      </Row>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
