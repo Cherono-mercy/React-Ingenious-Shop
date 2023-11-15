e4f8fcdf0bc89ef1bd1c221259cbb9ad0a053f75
Cherono-mercy <cheronom142@gmail.com>
2023-11-15 10:04:08 +0800

    Completed

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useThemeHook } from "../GlobalComponents/ThemeProvider";
 import Lightbox from "react-lightbox-component";
-
+import "react-lightbox-component/style.css";
 import "./product-details.css";
 import { useCart } from "react-use-cart";
 import { BsCartPlus } from "react-icons/bs";
